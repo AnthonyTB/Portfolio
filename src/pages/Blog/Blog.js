3548fd3069edec4b488227c9644ae595f5d2b1d0
AnthonyTB <anthonylol@protonmail.com>
2020-03-30 21:34:15 -0700

    started on blog route and initially setup a fetch call to grab blog with the id from route params

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Blog.css';
 
 export default function Blog() {
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    const fetchBlog = () => {
+      // api call to grab blog from route params
+    };
+  });
+
   return <div className='Blog'></div>;
 }

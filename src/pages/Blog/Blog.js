52892c0c809a26a339d68f3b6e59cc1aa8d03572
AnthonyTB <anthonylol@protonmail.com>
2020-03-31 22:41:33 -0700

    continued work on rendering specific blog

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './Blog.css';
+import BlogCalls from '../../Helpers/BlogCalls';
 
-export default function Blog() {
+export default function Blog(props) {
   const [blog, setBlog] = useState(null);
 
   useEffect(() => {
     const fetchBlog = () => {
+      BlogCalls.getBlog(props.match.params.blog_id);
       // api call to grab blog from route params
     };
   });

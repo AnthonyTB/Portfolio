cd6e3957d1dc5d027cb39f54246888cb3ddb90d5
AnthonyTB <anthonylol@protonmail.com>
2020-04-03 21:53:23 -0700

    scraped storing blogs on db instead using medium

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import './App.css';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import Home from './pages/Home/Home';
-import Blog from './pages/Blog/Blog';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route
-          path='/:blog-name'
-          render={routeProps => <Blog {...routeProps} />}
-        />
       </Switch>
     </Router>
   );

a97177c5d90fbcd0b289a317f7480fb0e7b7b01e
AnthonyTB <anthonylol@protonmail.com>
2020-04-04 20:29:52 -0700

    removed unneeded api call

--- src/Helpers/BlogCalls.js
+++ src/Helpers/BlogCalls.js
@@ -6,23 +6,12 @@ const BlogHelper = {
     return fetch(`${config.API_ENDPOINT}/blogs`, {
       method: 'GET',
       headers: {
-        'content-type': 'application/json'
-      }
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+        'content-type': 'application/json',
+      },
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
-  // api call that returns a specific blogs data
-  getBlog(blog_id) {
-    return fetch(`${config.API_ENDPOINT}/blogs/${blog_id}`, {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json'
-      }
-    }).then(res =>
-      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-    );
-  }
 };
 
 export default BlogHelper;
